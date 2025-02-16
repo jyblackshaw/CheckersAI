f19250b7d009230c77707885692f02fc9c0a9767
Julian Blackshaw <jblacksh@uci.edu>
2025-02-16 00:23:25 +0000

    got basic game working

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -1,10 +1,8 @@
 from random import randint
 from BoardClasses import Move
 from BoardClasses import Board
-#The following part should be completed by students.
-#Students can modify anything except the class name and exisiting functions and varibles.
-class StudentAI():
 
+class StudentAI():
     def __init__(self,col,row,p):
         self.col = col
         self.row = row
@@ -14,14 +12,37 @@ class StudentAI():
         self.color = ''
         self.opponent = {1:2,2:1}
         self.color = 2
+        
     def get_move(self,move):
         if len(move) != 0:
             self.board.make_move(move,self.opponent[self.color])
         else:
             self.color = 1
+            
         moves = self.board.get_all_possible_moves(self.color)
-        index = randint(0,len(moves)-1)
-        inner_index =  randint(0,len(moves[index])-1)
+        
+        # Try to find a capturing move first
+        best_move = None
+        max_captures = 0
+        
+        for i in range(len(moves)):
+            for j in range(len(moves[i])):
+                current_move = moves[i][j]
+                # If move sequence length > 2, it's a capturing move
+                if len(current_move.seq) > 2:
+                    captures = (len(current_move.seq) - 2) // 2
+                    if captures > max_captures:
+                        max_captures = captures
+                        best_move = (i, j)
+        
+        # If we found a capturing move, use it
+        if best_move is not None:
+            index, inner_index = best_move
+        else:
+            # Otherwise, choose randomly like before
+            index = randint(0,len(moves)-1)
+            inner_index = randint(0,len(moves[index])-1)
+        
         move = moves[index][inner_index]
         self.board.make_move(move,self.color)
         return move
